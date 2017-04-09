51becc6678051b2391992a5720d991b03c83af79
shortnotion <dr.theman@gmail.com>
2017-04-09 17:28:31 +0200

    LambdaEvent cleans up after itself

--- src/ofxUiEditor/LambdaEvent.h
+++ src/ofxUiEditor/LambdaEvent.h
@@ -11,6 +11,10 @@ namespace ofxUiEditor {
     template<typename Type>
     class LambdaEvent : public ofEvent<Type>, ofxLiquidEvent<Type>  {
 
+    public: // constructor etc.
+        ~LambdaEvent(){ destroy(); }
+        void destroy();
+
     public: // ofEvent method
         inline void notify(const void* sender, Type & param);
 
@@ -21,17 +25,30 @@ namespace ofxUiEditor {
         
     public: // custom methods
         void forward(ofEvent<Type> &event);
+        void stopForward(ofEvent<Type> &event);
+
         void forwardTo(ofEvent<Type> &event);
+        void stopForwardTo(ofEvent<Type> &event);
 
         void onForwardSource(Type &arg);
 
     private: // attributes
-        std::vector<ofEvent<Type>> forwardToEvents;
-        std::vector<ofEvent<Type>> forwardFromEvents;
+        std::vector<ofEvent<Type>*> forwardToEvents;
+        std::vector<ofEvent<Type>*> forwardFromEvents;
     };
 }
 
 using namespace ofxUiEditor;
+
+
+template<typename Type>
+inline void LambdaEvent<Type>::destroy(){
+    for(auto& event : forwardFromEvents)
+        stopForward(*event);
+    
+    forwardFromEvents.clear();
+    forwardToEvents.clear();
+}
 
 // overwrite ofEvent method; call original implementation AND call lambda notifications
 template<typename Type>
@@ -44,7 +61,7 @@ inline void LambdaEvent<Type>::notify(const void* sender, Type & param){
 template<typename Type>
 void LambdaEvent<Type>::notifyListeners(Type& arguments) {
     ofxLiquidEvent<Type>::notifyListeners(arguments);
-    ofEvent<Type>::notify(arguments);
+    ofEvent<Type>::notify(this, arguments);
 }
 
 // overwrite ofxLiquidEvent method; call original implementation AND call ofEvent notifications
@@ -61,9 +78,15 @@ void LambdaEvent<Type>::notifyListenersInReverse(Type& arguments) {
     ofEvent<Type>::notify(arguments);
 }
 
+
 template<typename Type>
 void LambdaEvent<Type>::forwardTo(ofEvent<Type> &event){
-    forwardToEvents.push_back(event);
+    forwardToEvents.push_back(&event);
+}
+
+template<typename Type>
+void LambdaEvent<Type>::stopForwardTo(ofEvent<Type> &event){
+    forwardToEvents.remove(&event);
 }
 
 template<typename Type>
@@ -71,7 +94,13 @@ void LambdaEvent<Type>::forward(ofEvent<Type> &event){
     // register listener function
     ofAddListener(event, this, &LambdaEvent<Type>::onForwardSource);
     // keep a record of events we're following
-    forwardFromEvents.push_back(event);
+    forwardFromEvents.push_back(&event);
+}
+
+template<typename Type>
+void LambdaEvent<Type>::stopForward(ofEvent<Type> &event){
+    ofRemoveListener(event, this, &LambdaEvent<Type>::onForwardSource);
+    forwardFromEvents.push_back(&event);
 }
 
 template<typename Type>
