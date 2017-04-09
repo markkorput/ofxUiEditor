df878dc25ad6b308ffe3635ecfa56c0d4f2f2389
shortnotion <dr.theman@gmail.com>
2017-04-09 17:21:11 +0200

    LambdaEvent .forward and .forwardTo methods

--- src/ofxUiEditor/LambdaEvent.h
+++ src/ofxUiEditor/LambdaEvent.h
@@ -1,20 +1,33 @@
 #pragma once
 
-#include "ofEvent.h" // ofEvents
-#include "ofxLiquidEvent.h" // lambda event
+// OF
+#include "ofEvent.h"
+#include "ofEventUtils.h"
+// lambda event
+#include "ofxLiquidEvent.h"
 
 namespace ofxUiEditor {
 
     template<typename Type>
     class LambdaEvent : public ofEvent<Type>, ofxLiquidEvent<Type>  {
 
-    public: // ofEvent
+    public: // ofEvent method
         inline void notify(const void* sender, Type & param);
 
     public: // ofxLiquidEvent methods
         void notifyListeners();
         void notifyListeners(Type& arguments);
         void notifyListenersInReverse(Type& arguments);
+        
+    public: // custom methods
+        void forward(ofEvent<Type> &event);
+        void forwardTo(ofEvent<Type> &event);
+
+        void onForwardSource(Type &arg);
+
+    private: // attributes
+        std::vector<ofEvent<Type>> forwardToEvents;
+        std::vector<ofEvent<Type>> forwardFromEvents;
     };
 }
 
@@ -46,4 +59,22 @@ template<typename Type>
 void LambdaEvent<Type>::notifyListenersInReverse(Type& arguments) {
     ofxLiquidEvent<Type>::notifyListenersInReverse(arguments);
     ofEvent<Type>::notify(arguments);
+}
+
+template<typename Type>
+void LambdaEvent<Type>::forwardTo(ofEvent<Type> &event){
+    forwardToEvents.push_back(event);
+}
+
+template<typename Type>
+void LambdaEvent<Type>::forward(ofEvent<Type> &event){
+    // register listener function
+    ofAddListener(event, this, &LambdaEvent<Type>::onForwardSource);
+    // keep a record of events we're following
+    forwardFromEvents.push_back(event);
+}
+
+template<typename Type>
+void LambdaEvent<Type>::onForwardSource(Type &arg){
+    notifyListeners(arg);
 }
