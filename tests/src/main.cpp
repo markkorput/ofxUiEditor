f8ef9ebcd0f8fba9180b10cfcfd7b5b604a9f5d3
Mark van de Korput <mark.korput@gmail.com>
2017-04-14 00:05:06 +0200

    test-date and desired api

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -1,7 +1,27 @@
 #include "ofxUnitTests.h"
+#include "../../src/ofxUiEditor.h"
 
 class ofApp: public ofxUnitTestsApp{
     void run(){
+        // StructureManager
+        string filename = "structures.xml";
+        ofxUiEditor::StructureManager strucman;
+        strucman.setup(filename);
+
+        // TODO; StructureManager unit-tests?
+
+        // Editor with StructureManager
+        ofxUiEditor::Editor editor;
+        editor.use(strucman);
+        shared_ptr<ofxInterface::Node> nodeRef = editor.create("window");
+        test_eq(nodeRef->getName(), "window", "name");
+        auto& children =  nodeRef->getChildren();
+        test_eq(children.get(0)->getName(), "titlebar");
+        test_eq(children.get(0)->getChildren().get(0)->getName(), "title");
+        test_eq(children.get(0)->getChildren().get(1)->getName(), "close");
+        test_eq(children.get(1)->getName(), "message");
+        test_eq(children.get(2)->getName(), "cancel");
+        test_eq(children.get(3)->getName(), "submit");
     }
 };
 
